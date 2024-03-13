a3ee53423dbbbef08be9a6fb811b322247811f15
effan-hub <effan.android@gmail.com>
2024-03-13 10:54:43 +0700

    ubah index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Effan</title>
+</head>
+<body>
+    <h1>Effan Najwani</h1>
+    <h2>nim: e23020202</h2>
+</body>
+</html>
